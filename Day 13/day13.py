0171a7823cfda280c023ea047f08e8e3cbf65b6e
Okthia103 <137489181+Okthia103@users.noreply.github.com>
2024-07-03 07:34:05 +0700

    Add files via upload

---
+++ Day 13/day13.py
@@ -0,0 +1,22 @@
+a = 10
+b = 5
+
+# Penjumlahan
+c = a + b
+print(f"{a} + {b} = {c}")
+
+# Pengurangan
+c = a - b
+print(f"{a} - {b} = {c}")
+
+# Perkalian
+c = a * b
+print(f"{a} * {b} = {c}")
+
+# Pembagian
+c = a / b
+print(f"{a} / {b} = {c}")
+
+# Modulus (sisa bagi)
+c = a % b
+print(f"{a} % {b} = {c}")
